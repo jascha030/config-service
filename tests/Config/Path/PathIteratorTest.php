7391f4aa5ee020ff9455c422a3ae6c68bb282014
Jascha030 <contact@jaschavanaalst.nl>
2022-04-03 22:40:54 +0200

    ✅ Add unit tests for path iterator

--- tests/Config/Path/PathIteratorTest.php
+++ tests/Config/Path/PathIteratorTest.php
@@ -1,11 +1,50 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Config\Tests\Config\Path;
 
+use Exception;
+use IteratorAggregate;
 use Jascha030\Config\Config\Path\PathIterator;
 use PHPUnit\Framework\TestCase;
 
-class PathIteratorTest extends TestCase
+/**
+ * @internal
+ * @covers \Jascha030\Config\Config\Path\PathIterator
+ */
+final class PathIteratorTest extends TestCase
 {
+    public static ?string $configPath = null;
 
+    public static function setUpBeforeClass(): void
+    {
+        self::$configPath = sprintf('%s/.config', getenv('HOME'));
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$configPath = null;
+    }
+
+    public function testConstruct(): void
+    {
+        $this->assertInstanceOf(
+            IteratorAggregate::class,
+            new PathIterator(self::$configPath, [])
+        );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetIterator(): void
+    {
+        $paths     = ['path1', 'path2'];
+        $aggregate = new PathIterator(self::$configPath, $paths);
+        $array     = iterator_to_array($aggregate);
+
+        $this->assertEquals($array, iterator_to_array($aggregate->getIterator()));
+        $this->assertEquals(reset($array), sprintf('%s/%s', self::$configPath, $paths[0]));
+    }
 }
