c6d910f1e4d39d34af5ce1ad8057e7d2fdff6b24
Jascha030 <contact@jaschavanaalst.nl>
2022-04-03 23:48:12 +0200

    ♻️ Remove directory from iterator, as it shouldn't have to be it's responsibility.

---
+++ src/Config/Traits/TemplateDirectoryTrait.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Jascha030\Config\Config\Traits;
+
+trait TemplateDirectoryTrait
+{
+
+}
---
+++ tests/Config/Traits/ConsoleApplicationConfigTraitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jascha030\Config\Tests\Config\Traits;
+
+use Jascha030\Config\Config\Traits\ConsoleApplicationConfigTrait;
+use PHPUnit\Framework\TestCase;
+
+class ConsoleApplicationConfigTraitTest extends TestCase
+{
+
+}
