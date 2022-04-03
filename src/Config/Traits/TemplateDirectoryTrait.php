51d6fe89a31f7b996ccbb81a7ffefb445bf4579f
Jascha030 <contact@jaschavanaalst.nl>
2022-04-03 23:50:10 +0200

    ✨ Add trait for creating config files from a template directory.

--- src/Config/Traits/TemplateDirectoryTrait.php
+++ src/Config/Traits/TemplateDirectoryTrait.php
@@ -1,8 +1,51 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Config\Config\Traits;
+
+use Generator;
+use IteratorAggregate;
+use Jascha030\Config\Config\Path\ConfigIterator;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 trait TemplateDirectoryTrait
 {
+    abstract public function getName(): string;
 
+    abstract private function getTemplateDir(): string;
+
+    public function getFiles(): ConfigIterator
+    {
+        return new ConfigIterator(
+            $this->getName(),
+            iterator_to_array($this->getPaths())
+        );
+    }
+
+    /**
+     * @noinspection PhpUnnecessaryLocalVariableInspection
+     */
+    private function getPaths(): Generator
+    {
+        foreach ($this->getFinder() as $file) {
+            /** @var SplFileInfo $file */
+            $path = str_replace(
+                $this->getTemplateDir(),
+                '',
+                $file->getRealPath()
+            );
+
+            yield $path => $file->getContents();
+        }
+    }
+
+    private function getFinder(): IteratorAggregate
+    {
+        return (Finder::create()
+            ->in($this->getTemplateDir())
+            ->files()
+            ->getIterator())();
+    }
 }
