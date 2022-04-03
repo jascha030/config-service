155bbe6ded46cf884ed40eb518d874fc9de7e038
Jascha030 <contact@jaschavanaalst.nl>
2022-04-03 23:49:01 +0200

    ✨ Add trait to create config dirs specifically for symfony/console Applications.

--- src/Config/Traits/ConsoleApplicationConfigTrait.php
+++ src/Config/Traits/ConsoleApplicationConfigTrait.php
@@ -1,8 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Config\Config\Traits;
+
+use Symfony\Component\Console\Application;
 
 trait ConsoleApplicationConfigTrait
 {
+    abstract private function getApplication(): Application;
 
+    public function getName(): string
+    {
+        return $this->getApplication()->getName();
+    }
 }
